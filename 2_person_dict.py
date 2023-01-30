3d563f7bb390005851cfe744428e195dc40f8ca9
Kyle Kahley <kyle_kahley1@baylor.edu>
2023-01-30 14:13:03 -0600

    changes 1/30 2

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,17 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person.get("children")[1])
+print(person['children'][1])#same thing
+
+
+print(type(person['pets']))
+print(person['pets']['cat'])
+for i in person ['children']:
+    print(i)
+
+    #print out the pets in this format;
+    #'type of pet: dog name of pet: Fido
+for i,j in person['pets'].items():
+    print(f"type of pet:{i} name of pet: {j}")
