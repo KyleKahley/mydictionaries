885cfd75b2832e36a893db73f01589a71e293677
Kyle Kahley <kyle_kahley1@baylor.edu>
2023-02-06 14:19:35 -0600

    class 2/5

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -17,3 +17,32 @@ Display report for all universities that have a total price for in-state student
 
 
 """
+import json
+infile = open('school_data.json', 'r')
+schools = json.load(infile)
+conference_schools = [372,108,107, 130]
+"""""
+print(type(schools))
+
+#how many schools are in this file? 
+print(len(schools)) #counting the number of dictionaries = number of schools 
+#print name school, graduation for conferences, graduation above 80% for women 
+for name_school in schools:
+    #print(name_school["instnm"]) shows names of all the schools
+    print(name)
+    conference = name_school["NCAA"]["NAIA conference number football (IC2020)"]
+    grad_rate_women = name_school[]
+    name = ["instnm"]
+    if conference = i in conference_schools:
+        print(name)
+        print(conference)
+"""""
+
+for name_school in schools:
+    if name_school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if name_school["Graduation rate  women (DRVGR2020)"] > 80: 
+            print(name_school["instnm"])
+            print(name_school["Graduation rate  women (DRVGR2020)"])
+            print()
+            print()
+    
